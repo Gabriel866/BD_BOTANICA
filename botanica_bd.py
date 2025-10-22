7169307dca99ba6eb6a4a3ff474c0db478137c82
Gariel Lugo <al22760840@ite.edu.mx>
2025-10-21 19:48:28 -0700

    first commit

---
+++ botanica_bd.py
@@ -0,0 +1,46 @@
+import psycopg2
+# Conexión a PostgreSQL
+conn = psycopg2.connect(
+    dbname="biologia_botanica",
+    user="postgres",        
+    password="Garoim76",  
+    host="localhost",
+    port="5432"
+)
+cur = conn.cursor()
+
+# Lista de palabras clave
+palabras = [
+    ("fotosíntesis", 98.5, "producción de energía, clorofila, luz solar"),
+    ("célula vegetal", 97.0, "citoplasma, núcleo, pared celular"),
+    ("clorofila", 96.2, "pigmento verde, fotosistema, energía solar"),
+    ("flor", 94.7, "pétalos, reproducción, planta"),
+    ("semilla", 93.5, "germinación, embrión, fruto"),
+    ("raíz", 92.8, "absorción, anclaje, rizoma"),
+    ("tallo", 91.6, "soporte, xilema, floema"),
+    ("hoja", 90.9, "estoma, cloroplasto, fotosíntesis"),
+    ("polinización", 89.8, "abeja, reproducción, floración"),
+    ("germinación", 88.6, "crecimiento, semilla, humedad"),
+    ("planta", 87.4, "organismo vegetal, flora"),
+    ("ecosistema", 86.2, "hábitat, biodiversidad, bioma"),
+    ("botánica", 85.0, "biología vegetal, taxonomía"),
+    ("floración", 84.3, "estación, primavera, reproducción"),
+    ("xilema", 83.0, "transporte, savia, conducción")
+]
+
+# Inserción de datos
+cur.executemany("""
+    INSERT INTO palabras_clave (palabra, porcentaje_identidad, sinonimos)
+    VALUES (%s, %s, %s);
+""", palabras)
+
+conn.commit()
+print(" Datos insertados correctamente.")
+
+# Verificación
+cur.execute("SELECT * FROM palabras_clave;")
+for fila in cur.fetchall():
+    print(fila)
+
+cur.close()
+conn.close()
